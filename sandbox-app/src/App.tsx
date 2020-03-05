f93e85bb2adbc991209803a32ed40e58b7788005
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-03-05 22:36:30 +0100

    Fix

--- sandbox-app/src/App.tsx
+++ sandbox-app/src/App.tsx
@@ -4,21 +4,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="App">
+        <p>
+            EnMasse Sandbox is a community service offering for developers who want to try EnMasse without setting up Kubernetes and installing Enmasse.
+        </p>
+        <p>
+            You can use a <a href="https://www.github.com">GitHub</a> account to register and get an environment created for 7 days. Once expired, all data for the environment and account data will be deleted.
+        </p>
+        <a className="link" href="/register">Register</a>
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        <a className="link" href="/dashboard">Dashboard</a>
+      </div>
     </div>
   );
 }
