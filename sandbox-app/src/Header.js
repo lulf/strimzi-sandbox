6cb9352af1affd06a1c2404aac3af41f85a38410
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-03-22 22:21:19 +0100

    Improve wording

--- sandbox-app/src/Header.js
+++ sandbox-app/src/Header.js
@@ -14,7 +14,7 @@ class Header extends Component {
                 EnMasse Sandbox is a community service offering for developers who want to try EnMasse without setting up Kubernetes and installing Enmasse.
             </p>
             <p>
-                You can use a <a href="https://www.github.com">GitHub</a> account to register and get an environment created for 7 days. Once expired, all data for the environment and account data will be deleted.
+                You can use a <a href="https://www.github.com">GitHub</a> account to register and get a free environment for a limited time period. Once expired, all data for the environment and account data will be deleted.
             </p>
         </div></div>);
   }
