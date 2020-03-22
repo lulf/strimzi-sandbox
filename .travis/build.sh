815d71ed9b867bd491acdcce11f0b78e430ffdbe
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-03-22 14:10:46 +0100

    Use docker to build

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,5 +3,5 @@ set -ex
 
 mvn package
 
-podman build -f src/main/docker/Dockerfile.jvm -t sandbox-api:latest sandbox-api
-podman build -f src/main/docker/Dockerfile.jvm -t sandbox-operator:latest sandbox-operator
+docker build -f src/main/docker/Dockerfile.jvm -t sandbox-api:latest sandbox-api
+docker build -f src/main/docker/Dockerfile.jvm -t sandbox-operator:latest sandbox-operator
