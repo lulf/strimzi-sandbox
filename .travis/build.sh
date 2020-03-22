c8f7c439799561cd62039fbcd50a4ff78aeb7e56
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-03-22 14:36:58 +0100

    Add dockerfile

--- .travis/build.sh
+++ .travis/build.sh
@@ -12,3 +12,4 @@ mvn package
 
 podman build -f src/main/docker/Dockerfile.jvm -t sandbox-api:latest sandbox-api
 podman build -f src/main/docker/Dockerfile.jvm -t sandbox-operator:latest sandbox-operator
+podman build -f Dockerifle -t sandbox-app:latest sandbox-app
