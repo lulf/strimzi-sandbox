4d81aec6a320b393b2f132141e234dc3bafeee0e
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-03-22 14:37:55 +0100

    Add setup scripts

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-# Setup container build env
-. /etc/os-release
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | sudo apt-key add -
-sudo apt-get update -qq
-sudo apt-get -qq -y install podman
-
 mvn package
 
 podman build -f src/main/docker/Dockerfile.jvm -t sandbox-api:latest sandbox-api
---
+++ .travis/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -ex
+
+# Setup container build env
+. /etc/os-release
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | sudo apt-key add -
+sudo apt-get update -qq
+sudo apt-get -qq -y install podman
