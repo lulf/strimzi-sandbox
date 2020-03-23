c3638e7ac6bc3138f3b757b3137949fad4af1d45
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-03-23 13:36:37 +0100

    Update looks

--- sandbox-app/src/Header.js
+++ sandbox-app/src/Header.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class Header extends Component {
   render() {
       return (
-              <div>
         <div className="App">
-        <header>
             <img src={logo} className="App-logo" alt="logo" />
-        </header>
-              </div>
-        <div className="App">
             <p>
                 EnMasse Sandbox is a public service for developers who want to try a managed EnMasse service without having to setting up Kubernetes.
             </p>
             <p>
                 You can use a <a href="https://www.github.com">GitHub</a> account to register and get a free environment for a limited time period. Once expired, all data for the environment and account data will be deleted.
             </p>
-        </div></div>);
+        </div>);
   }
 };
 
