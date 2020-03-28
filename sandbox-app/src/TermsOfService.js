cdadca7aa5726dc2cddca09890ce4cdc0496a98e
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-03-28 17:33:53 +0100

    Improve ToS

--- sandbox-app/src/TermsOfService.js
+++ sandbox-app/src/TermsOfService.js
@@ -8,7 +8,7 @@ class TermsOfService extends Component {
               <h3>Note</h3>
               <ul>
               <li>The service is intended for <b>testing purposes only</b>. Under no circumstances should it be used for any production use case.</li>
-              <li>It is <b>not allowed</b> to register with or publish any personally identifiable information to any of the sandbox services.</li>
+              <li>It is <b>not allowed</b> to publish any personally identifiable information to any of the sandbox services.</li>
               <li>We do not collect nor share with third parties any of the data you provide when registering.</li>
               <li>The sandbox will be running the latest EnMasse master build that may be updated without further notice.</li>
               <li>Everyone who knows your tenant identifier will be able to produce and consume data to your instance.</li>
