e377ea53ee61f2e73c19d9c330dcf0b019258cc1
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-08-05 10:12:35 +0200

    Fix docker build commands

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,6 +3,6 @@ set -ex
 
 mvn package
 
-docker build -f src/main/docker/Dockerfile.jvm -t sandbox-api:latest sandbox-api
-docker build -f src/main/docker/Dockerfile.jvm -t sandbox-operator:latest sandbox-operator
-docker build -f Dockerfile -t sandbox-app:latest sandbox-app
+docker build -f sandbox-api/src/main/docker/Dockerfile.jvm -t sandbox-api:latest sandbox-api
+docker build -f sandbox-operator/src/main/docker/Dockerfile.jvm -t sandbox-operator:latest sandbox-operator
+docker build -f sandbox-app/Dockerfile -t sandbox-app:latest sandbox-app
